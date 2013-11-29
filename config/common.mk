560f7f907f7553cff4602a7beb40bd15d1618d28
kowcop <kowcop+@gmail.com>
2013-11-30 09:07:53 +1100

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -1,4 +1,14 @@
 PRODUCT_BRAND ?= kow
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=dd-MM-yyyy \
+    ro.com.android.dataroaming=false
+
 # Add our overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/kow/overlay/common
